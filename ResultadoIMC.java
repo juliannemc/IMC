7d4c4e9817263a1ec2d8b6fbaadf1b2a22c0e1db
juliannemc <juliannecavalcanti@gmail.com>
2016-04-16 16:10:27 -0300

    Adicao do Classe Java ResultadoIMC

---
+++ ResultadoIMC.java
@@ -0,0 +1,9 @@
+public class ValorIMC { 
+
+
+public static void main (String [] args){ 
+CalculoImc pessoa1 = new CalculoImc(170, 75.0); 
+
+System.out.println ("Seu resultado é: " +pessoa1.calculoIMC()); 
+} 
+} 
